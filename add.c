dabb7511a99bbc983caa5414a42a44bff1815a28
Syed Imaduddin <imaduddinsyed09@gmail.com>
2024-03-08 12:46:00 +0530

    Add files via upload

---
+++ add.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b;
+
+    printf("\nEnter the value of a : ");
+    scanf("%d", &a);
+
+    printf("\nEnter the value of b : ");
+    scanf("%d", &b);
+
+    printf("\nSum of a and b is : %d \n", a+b);
+
+
+    return 0;
+}
